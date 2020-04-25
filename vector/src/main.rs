5b2c776cfe6fb39178d9fceac55d5cf2aa279b85
hirontan <hirontan@sora.flights>
2020-04-25 10:44:21 +0900

    UTF-8 エンコードされたテキストを文字列で保存する

--- vector/src/main.rs
+++ vector/src/main.rs
@@ -63,4 +63,83 @@ fn main() {
         SpreadsheetCell::Text(String::from("blue")),
         SpreadsheetCell::Float(10.12),
     ];
+
+    let mut s = String::new();
+
+    let data = "initial contents";
+    println!("{}", data);
+    
+    let s = data.to_string();
+    println!("{}", s);
+    
+    // the method also works on a literal directly:
+    let s = "initial contents".to_string();
+    
+    let s = String::from("initial contents");
+    println!("{}", s);
+
+    let hello = String::from("السلام عليكم");
+    println!("{}", hello);
+    let hello = String::from("Dobrý den");
+    println!("{}", hello);
+    let hello = String::from("Hello");
+    println!("{}", hello);
+    let hello = String::from("שָׁלוֹם");
+    println!("{}", hello);
+    let hello = String::from("नमस्ते");
+    println!("{}", hello);
+    let hello = String::from("こんにちは");
+    println!("{}", hello);
+    let hello = String::from("안녕하세요");
+    println!("{}", hello);
+    let hello = String::from("你好");
+    println!("{}", hello);
+    let hello = String::from("Olá");
+    println!("{}", hello);
+    let hello = String::from("Здравствуйте");
+    println!("{}", hello);
+    let hello = String::from("Hola");
+    println!("{}", hello);
+
+    let mut s = String::from("foo");
+    s.push_str("bar");
+    println!("{}", s);
+
+    let mut s1 = String::from("foo");
+    let s2 = "bar";
+    s1.push_str(s2);
+    println!("s2 is {}", s2);
+
+    let mut s = String::from("lo");
+    s.push('l');
+    println!("{}", s);
+
+    let s1 = String::from("Hello, ");
+    let s2 = String::from("world!");
+    let s3 = s1 + &s2; // s1はムーブされ、もう使用できないことに注意
+    println!("{}", s3);
+
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+    let s = s1 + "-" + &s2 + "-" + &s3;
+    println!("{}", s);
+    
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+    let s = format!("{}-{}-{}", s1, s2, s3);
+    println!("{}", s);
+
+    let hello = "Здравствуйте";
+    let s = &hello[0..4];
+    println!("{}", s);
+
+    for c in "नमस्ते".chars(){
+        println!("{}", c);
+    }
+    
+    for b in "नमस्ते".bytes() {
+        println!("{}", b);
+    }
 }
