86a6fead0aa20dc8e9269c3a778c51651f57ace2
hirontan <hirontan@sora.flights>
2021-09-16 19:02:28 +0900

    [Add] サブモジュールを軽症

--- lesson/src/main.rs
+++ lesson/src/main.rs
@@ -3,4 +3,5 @@ mod vars;
 fn main() {
     println!("Hello, world!");
     vars::run();
+    vars::sub_a::func_a();
 }
--- lesson/src/vars.rs
+++ lesson/src/vars.rs
@@ -1,5 +1,6 @@
-mod sub_a;
-mod sub_b;
+// varsの親であるmainからも呼び出す場合、パブリックを設定
+pub mod sub_a;
+pub mod sub_b;
 
 // デフォルトではプライベート
 pub fn run() {
