ea8847f59e21e9ac19a64d361466f05c493d031e
hirontan <hirontan@sora.flights>
2021-09-20 11:36:58 +0900

    [Update] Genericsを活用して、関数定義

--- lesson/src/generics.rs
+++ lesson/src/generics.rs
@@ -1,11 +1,29 @@
 pub fn run() {
   let number_list = vec![34, 50, 25, 100, 101];
 
-  println!("The largest: {}", largest_i32(number_list));
+  // 所有権が移動しているので、コメントアウト
+  // println!("The largest: {}", largest_i32(number_list));
+
+  let char_list = vec!['a', 'b', 'c', 'd'];
+  println!("The largest of char_list: {}", largest(char_list));
+  println!("The largest of number_list: {}", largest(number_list));
 }
 
 // Vector型で格納された数値の最大値を取得する
 fn largest_i32(list: Vec<i32>) -> i32 {
+  let mut largest = list[0];
+  for item in list {
+    if item > largest {
+      largest = item;
+    }
+  }
+  largest
+}
+
+// Genericsを活用して、関数定義
+// <T>を指定するとあらゆるデータ型を取得できる
+// Trait境界: 比較が可能なデータ型をGenericsとして許容する（今回は、PartialOrdとCopyを利用）
+fn largest<T: PartialOrd + Copy>(list: Vec<T>) -> T {
   let mut largest = list[0];
   for item in list {
     if item > largest {
