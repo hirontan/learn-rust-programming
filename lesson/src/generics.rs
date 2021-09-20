8b64ae09b80d2bb650b05539a1bedf0239b3db52
hirontan <hirontan@sora.flights>
2021-09-20 11:27:08 +0900

    [Update] 「Vector型で格納された数値の最大値を取得する」を関数切り出し

--- lesson/src/generics.rs
+++ lesson/src/generics.rs
@@ -1,12 +1,16 @@
 pub fn run() {
   let number_list = vec![34, 50, 25, 100, 101];
-  let mut largest = number_list[0];
 
-  // Vector型で格納された数値の最大値を取得する
-  for number in number_list {
-    if number > largest {
-      largest = number;
+  println!("The largest: {}", largest_i32(number_list));
+}
+
+// Vector型で格納された数値の最大値を取得する
+fn largest_i32(list: Vec<i32>) -> i32 {
+  let mut largest = list[0];
+  for item in list {
+    if item > largest {
+      largest = item;
     }
   }
-  println!("The largest: {}", largest);
+  largest
 }
