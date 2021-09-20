c3e189fd473c277d0d8435b2caa6a840fa0f7ca0
hirontan <hirontan@sora.flights>
2021-09-20 11:23:22 +0900

    [Update] Vector型で格納された数値の最大値を取得する

--- lesson/src/generics.rs
+++ lesson/src/generics.rs
@@ -1,1 +1,12 @@
-pub fn run() {}
+pub fn run() {
+  let number_list = vec![34, 50, 25, 100, 101];
+  let mut largest = number_list[0];
+
+  // Vector型で格納された数値の最大値を取得する
+  for number in number_list {
+    if number > largest {
+      largest = number;
+    }
+  }
+  println!("The largest: {}", largest);
+}
