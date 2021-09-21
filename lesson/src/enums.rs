2c2afbe95149a57b386da071966e4591f0d759ec
hirontan <hirontan@sora.flights>
2021-09-22 08:37:17 +0900

    [Update] 実体を作成

--- lesson/src/enums.rs
+++ lesson/src/enums.rs
@@ -4,4 +4,8 @@ enum OS {
   Linux(u32, String),
 }
 
-pub fn run() {}
+pub fn run() {
+  let linux = OS::Linux(1991, String::from("Linus"));
+  let windows = OS::Windows(1985, String::from("MicroSoft"));
+  let apple = OS::Mac(2001, String::from("Apple"));
+}
