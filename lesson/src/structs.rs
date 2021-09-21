bdf67b17b89bb4146b7b6eb7559fa951a3d9e47d
hirontan <hirontan@sora.flights>
2021-09-22 08:09:25 +0900

    [Update] MutableなUserの実体

--- lesson/src/structs.rs
+++ lesson/src/structs.rs
@@ -12,4 +12,12 @@ pub fn run() {
     sign_in_count: 1,
     active: true,
   };
+  // let user2 = user1; // 所有権が移動する
+  let mut user1 = User {
+    username: String::from("test"),
+    email: String::from("test@example.com"),
+    sign_in_count: 1,
+    active: true,
+  };
+  user1.email = String::from("test_update@example.com");
 }
