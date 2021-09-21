9c69884f80136736d408c4a4a35505dc703f90ea
hirontan <hirontan@sora.flights>
2021-09-21 11:45:23 +0900

    [Update] 大きい方の参照を返す関数

--- lesson/src/lifetime.rs
+++ lesson/src/lifetime.rs
@@ -2,3 +2,12 @@ pub fn run() {
   let st1 = String::from("x");
   let st2 = String::from("y");
 }
+
+// 大きい方の参照を返す関数
+fn get_longest(x: &str, y: &str) -> &str {
+  if x.len() > y.len() {
+    x
+  } else {
+    y
+  }
+}
