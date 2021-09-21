40f2919b39c1ccc88f242606a78bb34ac401836f
hirontan <hirontan@sora.flights>
2021-09-22 08:40:11 +0900

    [Update] OSの種類に応じて実体を切り替える関数

--- lesson/src/enums.rs
+++ lesson/src/enums.rs
@@ -9,3 +9,18 @@ pub fn run() {
   let windows = OS::Windows(1985, String::from("MicroSoft"));
   let apple = OS::Mac(2001, String::from("Apple"));
 }
+
+// OSの種類に応じて実体を切り替える
+fn print_os_info(os: OS) {
+  match os {
+    OS::Windows(year, who) => {
+      println!("Windows: first release in {} by {}", year, who);
+    }
+    OS::Mac(year, who) => {
+      println!("Mac: first release in {} by {}", year, who);
+    }
+    OS::Linux(year, who) => {
+      println!("Linux: first release in {} by {}", year, who);
+    }
+  }
+}
