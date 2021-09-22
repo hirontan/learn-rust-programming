053eda3c353057efbb7972fefcfd8ae4a9b6ed15
hirontan <hirontan@sora.flights>
2021-09-22 14:50:12 +0900

    [Add] Traitsの骨子

--- lesson/src/main.rs
+++ lesson/src/main.rs
@@ -4,7 +4,8 @@
 // mod generics;
 // mod lifetime;
 // mod structs;
-mod enums;
+// mod enums;
+mod traits;
 
 fn main() {
     // println!("Hello, world!");
@@ -16,5 +17,6 @@ fn main() {
     // generics::run();
     // lifetime::run();
     // structs::run();
-    enums::run();
+    // enums::run();
+    traits::run();
 }
