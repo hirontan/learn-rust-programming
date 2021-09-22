f9fa62f699f1d2c7cfd08f1d4e824fd0e2f040a0
hirontan <hirontan@sora.flights>
2021-09-22 14:55:00 +0900

    [Update] 構造体の定義

--- lesson/src/traits.rs
+++ lesson/src/traits.rs
@@ -3,4 +3,18 @@ trait Fruits {
   fn price(&self) -> u32;
 }
 
+struct Apple;
+impl Fruits for Apple {
+  fn price(&self) -> u32 {
+    10
+  }
+}
+
+struct Banana;
+impl Fruits for Banana {
+  fn price(&self) -> u32 {
+    5
+  }
+}
+
 pub fn run() {}
