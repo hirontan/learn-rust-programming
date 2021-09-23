62e5a62a1eb743015d52979eeb10cdaa2249b14d
hirontan <hirontan@sora.flights>
2021-09-23 11:35:09 +0900

    [Delete] rust-lib-demoリポジトリに移動

--- lib_demo/src/generator.rs
+++
@@ -1,9 +0,0 @@
-// https://crates.io/crates/rand
-use rand::Rng;
-
-// ランダムの整数を生成
-pub fn get_ran() -> u8 {
-  let mut rng = rand::thread_rng();
-  let n: u8 = rng.gen();
-  n
-}
--- lib_demo/src/lib.rs
+++
@@ -1,6 +0,0 @@
-mod generator;
-
-pub fn print_random_number() {
-    let n = generator::get_ran();
-    println!("Rabdom number: {}", n);
-}
