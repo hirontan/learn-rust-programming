a01a4b9ed67ffd7b873a1cd16d6de1cac2fd3d1b
hirontan <hirontan@sora.flights>
2021-09-23 10:04:28 +0900

    [Update] 割り算の例外処理を行う関数

--- lesson/src/error_handling.rs
+++ lesson/src/error_handling.rs
@@ -1,1 +1,11 @@
 pub fn run() {}
+
+// Option: https://doc.rust-lang.org/std/option/enum.Option.html
+// 値が存在しない場合に例外処理を行う
+fn division_option(x: f64, y: f64) -> Option<f64> {
+  if y == 0.0 {
+    None
+  } else {
+    Some(x / y)
+  }
+}
