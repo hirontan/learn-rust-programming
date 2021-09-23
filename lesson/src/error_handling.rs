9e349fe9823a4e0b539387e87f61975f99588084
hirontan <hirontan@sora.flights>
2021-09-23 10:18:10 +0900

    [Update] division_optionの結果ごとの出力関数

--- lesson/src/error_handling.rs
+++ lesson/src/error_handling.rs
@@ -1,4 +1,7 @@
-pub fn run() {}
+pub fn run() {
+  let res1 = division_option(5.0, 2.0);
+  match_options(res1);
+}
 
 // Option: https://doc.rust-lang.org/std/option/enum.Option.html
 // 値が存在しない場合に例外処理を行う
@@ -7,5 +10,12 @@ fn division_option(x: f64, y: f64) -> Option<f64> {
     None
   } else {
     Some(x / y)
+  }
+}
+
+fn match_options(result: Option<f64>) {
+  match result {
+    Some(x) => println!("Result: {:.3}", x),
+    None => println!("Not allowed!!"),
   }
 }
